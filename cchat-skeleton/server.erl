6727b22b7ff4290a3c8d8f2674645039e3aaf36e
Paint19 <hanna@schaff.se>
2021-02-18 11:38:33 +0100

    refactored

--- cchat-skeleton/server.erl
+++ cchat-skeleton/server.erl
@@ -12,16 +12,6 @@ initial_state(Channels,Nicks) ->
       nicks = Nicks
     }.
 
--record(channel_st, {
-    name,           % name of this channel
-    clients=[]      % clients on this channel
-    }).
-
-initial_channel_state(ChannelAtom) ->
-    #channel_st{
-        name    = ChannelAtom,
-        clients = []
-}.
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
@@ -44,7 +34,7 @@ handle(ServerSt, {join, Channel, Client}) ->
     case lists:member(Channel, ServerSt#server_st.channels) of    
         false ->
             %start new channel
-            genserver:start(list_to_atom(Channel), initial_channel_state(Channel), fun channelhandle/2);
+            genserver:start(list_to_atom(Channel), channel:initial_state(Channel), fun channel:handle/2);
         true ->
             % channel already existing
             true
@@ -94,43 +84,3 @@ handle(ServerSt, {nick, OldNick, NewNick}) ->
 handle(ServerSt, stop_channels) ->
     lists:foreach(fun(Channel) -> genserver:stop(list_to_atom(Channel)) end, ServerSt#server_st.channels),
     {reply, ok, ServerSt}.
-
-
-% Join channel
-channelhandle(ChannelSt, {join, Client}) ->
-    case lists:member(Client, ChannelSt#channel_st.clients) of    
-        true -> 
-            %user has already joined this channel
-            {reply, failed, ChannelSt};
-        false ->
-            % user joines the channel
-            {reply, joined, ChannelSt#channel_st{clients=[Client | ChannelSt#channel_st.clients]}}
-    end;
-
-% Leave channel
-channelhandle(ChannelSt, {leave, Client}) ->
-    case lists:member(Client, ChannelSt#channel_st.clients) of    
-        true -> 
-            % user is in channel
-            NewChannelState = ChannelSt#channel_st{clients= lists:delete(Client,ChannelSt#channel_st.clients)},
-            % remove user from channel
-            {reply, left_channel, NewChannelState};
-        false ->
-            % User not joined
-            {reply, user_not_joined, ChannelSt}
-    end;
-
-% Sending message (from GUI, to channel)
-channelhandle(ChannelSt, {message_send, Msg, Client, Nick}) ->
-    case lists:member(Client, ChannelSt#channel_st.clients) of
-        false ->
-            % user hasn't join this channel
-            {reply, user_not_joined, ChannelSt};
-        true ->
-            % sends the message to everyone but the sender
-            spawn( fun() -> [genserver:request(Receiver,
-                {message_receive, ChannelSt#channel_st.name, Nick, Msg})
-                || Receiver <- ChannelSt#channel_st.clients, Receiver =/= Client]
-            end),
-            {reply, message_send, ChannelSt}
-    end.
